884b2579147620196b2460b57de6caee5132b603
nexusA <dongkyu91622@gmail.com>
2016-12-22 02:10:17 +0900

    add onEnable() - Patch 1

--- src/main/java/friend/Main.java
+++ src/main/java/friend/Main.java
@@ -1,10 +1,20 @@
 package friend;
 
+import java.io.File;
+
 import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
 
-public class Main extends PluginBase implements Listener{
-	
-	
+public class Main extends PluginBase implements Listener {
+
+	@Override
+	public void onEnable() {
+		this.getLogger().info("[Loading] Plugin Loading...");
+		this.getServer().getPluginManager().registerEvents(this, this);
+		this.getLogger().info("[Loading] EventHandler... success!");
+		Config data = new Config(new File(this.getDataFolder(), "friend.json"), Config.JSON);
+		this.getLogger().info("[Loading] Plugin Data Loading...");
+	}
 
 }
