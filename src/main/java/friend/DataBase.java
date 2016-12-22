8bb3404acab21de9dc7dcd5cf99aee908e6f1d88
nexusA <dongkyu91622@gmail.com>
2016-12-22 16:17:53 +0900

    add getFriend() ``` return ArrayList<Player>;// friendList
    ```

--- src/main/java/friend/DataBase.java
+++ src/main/java/friend/DataBase.java
@@ -1,8 +1,12 @@
 package friend;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+import cn.nukkit.Player;
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.utils.Config;
 
@@ -27,5 +31,17 @@ public class DataBase {
 		Config FriendDB = new Config(new File(plugin.getDataFolder(), "Friend.json"), Config.JSON);
 		FriendDB.setAll(this.FriendDB);
 		FriendDB.save();
+	}
+
+	@SuppressWarnings("unchecked")
+	public ArrayList<Player> getFriend(String player) {
+		ArrayList<Player> friend = new ArrayList<>();
+
+		for (String str : (List<String>) ((Map<String, Object>) this.FriendDB.get(player.toLowerCase())).get("friends")) {
+			friend.add(plugin.getServer().getPlayer(str));
+		}
+
+		return friend;
+
 	}
 }
