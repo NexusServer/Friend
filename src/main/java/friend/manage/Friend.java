4f3d9c33e7ff51537d1feb596fdc920daf3a921e
nexusA <dongkyu91622@gmail.com>
2016-12-23 22:57:02 +0900

    getFriends - 친구들 목록을 List<String>형으로 불러옵니다

--- src/main/java/friend/manage/Friend.java
+++ src/main/java/friend/manage/Friend.java
@@ -1,7 +1,9 @@
 package friend.manage;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import cn.nukkit.Player;
 import friend.DataBase;
 import friend.Main;
 
@@ -20,6 +22,10 @@ public class Friend {
 
 	public Manage getManage() {
 		return plugin.getManage();
+	}
+
+	public List<String> getFriends(String player) {
+		return (ArrayList<String>) this.getDataBase().FriendDB.get(player);
 	}
 
 	public boolean isFriend(String p1, String p2) {
